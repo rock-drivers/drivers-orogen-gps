59b7b7d8388ffe69d5cc41ba2739c23de9094bbf
DAGON PC 1 <cuslam@avalon-rear.dfki.uni-bremen.de>
2012-08-02 15:07:08 +0200

    updated gpsd_test.rb

--- scripts/gpsd_test.rb
+++ scripts/gpsd_test.rb
@@ -1,16 +1,31 @@
 
-require 'widget_grid'
+#require 'widget_grid'
 require 'orocos'
 Orocos.initialize
 
-widget_grid = WidgetGrid.new
+#widget_grid = WidgetGrid.new
 
 Orocos.run 'test_gpsd' do
   gpsd = Orocos::TaskContext.get 'Task'
-  gpsd.hostname ='fritsche'
+  gpsd.hostname ='localhost'
+
+
+
+#  origin = '/base/Position'
+#     origin.x = 64000
+#     origin.y = 0
+#     origin.z = 0
+  
+#  gpsd.origin = origin
+
   gpsd.configure
   gpsd.start
-  widget_grid.display gpsd
-  widget_grid.run
+#  widget_grid.display gpsd
+#  widget_grid.run
+
+loop do
+  sleep 0.1
+end
+
 end
 
