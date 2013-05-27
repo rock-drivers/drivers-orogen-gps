4b842d39c5dd0f40625e63152a0da00bedac7d3f
Matthias Goldhoorn <matthias.goldhoorn@dfki.de>
2013-05-27 13:27:13 +0200

    Make sure that all data's are read from the GPSd itself. Added some debugging functions.

--- tasks/GPSDTask.cpp
+++ tasks/GPSDTask.cpp
@@ -52,70 +52,77 @@ bool GPSDTask::startHook()
 
 void GPSDTask::updateHook()
 {
+ bool valid = true;
+ while(valid){
 #if GPSD_API_MAJOR_VERSION >= 5
-  if(gpsd_daemon->waiting(0))
-  {
-      gps_data_t* pdata = gpsd_daemon->read();
+      if(gpsd_daemon->waiting(0))
+      {
+          gps_data_t* pdata = gpsd_daemon->read();
 #else
-  if(gpsd_daemon->waiting())
-  {
-      gps_data_t* pdata = gpsd_daemon->poll();
+      if(gpsd_daemon->waiting())
+      {
+          gps_data_t* pdata = gpsd_daemon->poll();
 #endif
-      if (pdata) 
-      {
-        counter = 0;
-        gps::Solution solution;
-        solution.altitude =  pdata->fix.altitude;
-        solution.latitude =  pdata->fix.latitude;
-        solution.longitude =  pdata->fix.longitude;
-        solution.noOfSatellites =  pdata->satellites_used;
-        solution.geoidalSeparation = pdata->separation;
-        solution.deviationLongitude = pdata->fix.epx;
-        solution.deviationLatitude= pdata->fix.epy;
-        solution.deviationAltitude= pdata->fix.epv;
-        solution.time = base::Time::now();
-        solution.ageOfDifferentialCorrections = -1;
+          if (pdata) 
+          {
+            counter = 0;
+            gps::Solution solution;
+            solution.altitude =  pdata->fix.altitude;
+            solution.latitude =  pdata->fix.latitude;
+            solution.longitude =  pdata->fix.longitude;
+            solution.noOfSatellites =  pdata->satellites_used;
+            solution.geoidalSeparation = pdata->separation;
+            solution.deviationLongitude = pdata->fix.epx;
+            solution.deviationLatitude= pdata->fix.epy;
+            solution.deviationAltitude= pdata->fix.epv;
+            solution.time = base::Time::now();
+            solution.ageOfDifferentialCorrections = -1;
 
-        switch(pdata->fix.mode)
-        {
-        case MODE_NOT_SEEN:
-          solution.positionType = gps::NO_SOLUTION;
-          break;
-        
-        case MODE_NO_FIX:
-          solution.positionType = gps::NO_SOLUTION;
-          break;
+            switch(pdata->fix.mode)
+            {
+            case MODE_NOT_SEEN:
+              solution.positionType = gps::NO_SOLUTION;
+              break;
+            
+            case MODE_NO_FIX:
+              solution.positionType = gps::NO_SOLUTION;
+              break;
 
-        case MODE_2D:
-          solution.positionType = gps::AUTONOMOUS_2D;
-          break;
+            case MODE_2D:
+              solution.positionType = gps::AUTONOMOUS_2D;
+              break;
 
-        case MODE_3D:
-          solution.positionType = gps::AUTONOMOUS;
-          break;
+            case MODE_3D:
+              solution.positionType = gps::AUTONOMOUS;
+              break;
 
-        default:
-          solution.positionType = gps::INVALID;
-        }
-        solution.ageOfDifferentialCorrections = (int) pdata->status;
-        update(solution);
-        // at the moment no constallation info is saved
-        //
+            default:
+              solution.positionType = gps::INVALID;
+            }
+            solution.ageOfDifferentialCorrections = (int) pdata->status;
+            update(solution);
+            // at the moment no constallation info is saved
+            //
 
+          }
+          else
+          {         
+              if(++counter > 5){
+                    std::cerr << "[Error] poll error, got wrong values more than 100 times in sequence" << std::endl;
+                    return exception(IO_ERROR);
+              } else {
+                    std::cerr << "[Warning] poll error, got wrong values (" << counter << " times)" << std::endl;
+              }
+          }
       }
-      else
+      else{
+        counter++;
+        valid = false;
+      }if (counter > 5)
       {
-          std::cerr << "poll error" << std::endl;
-          return exception(IO_ERROR);
+          std::cerr << "[Error] poll error: gpsd is not responding" << std::endl;
+          return exception(IO_TIMEOUT);
       }
-  }
-  else
-    counter++;
-
-  if (counter > 100)
-  {
-      std::cerr << "poll error: gpsd is not responding" << std::endl;
-      return exception(IO_TIMEOUT);
   }
 }
 
